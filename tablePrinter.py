1217d4d4980d89c6188ea189fe0394c334af5652
dubiza <dubiZA@users.noreply.github.com>
2018-06-26 16:59:02 -0600

    Initial Commit. Needs work

---
+++ tablePrinter.py
@@ -0,0 +1,7 @@
+tableData = [['apples', 'oranges', 'cherries', 'banana'],
+             ['Alice', 'Bob', 'Carol', 'David'],
+             ['dogs', 'cats', 'moose', 'goose']]
+
+
+def printTable(data):
+    colWidths = [0] * len(data)
