781392dfa3ff0e8646c5d094c8fea393536fb0e1
dubiza <dubiZA@users.noreply.github.com>
2018-06-27 17:03:20 -0600

    Print rjust table
    Exercise from chapter 6. While I realize there may be more efficient ways to carry out some of the tasks here, I wanted to use things only learned in the book so far.
    
    Some examples I looked at from others while I was trying to figure this out used functions like max(), etc. I opted to use an if statement in determining the colWidths as max() has not been covered in the book yet.

--- tablePrinter.py
+++ tablePrinter.py
@@ -5,3 +5,18 @@ tableData = [['apples', 'oranges', 'cherries', 'banana'],
 
 def printTable(data):
     colWidths = [0] * len(data)
+
+    for i in range(len(colWidths)):
+        for j in range(len(data[i])):
+            if colWidths[i] < len(data[i][j]):
+                colWidths[i] = len(data[i][j])
+
+    for colNum in range(len(data[0])):
+        for rowNum in range(len(data)):
+            print(data[rowNum][colNum].rjust(colWidths[rowNum]), end=' ')
+        print('\n')
+
+
+
+
+printTable(tableData)
